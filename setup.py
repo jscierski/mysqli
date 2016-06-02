1d3368871aaf14c2cd991836441a3ee4a9756620
Jacek Szpot <maligree@gmail.com>
2016-06-02 11:06:21 +0200

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='mysqli',
-    version='0.1.1',
+    version='0.2.0',
     author='Jacek Szpot',
     author_email='maligree@gmail.com',
     url='https://github.com/exana/mysqli',
