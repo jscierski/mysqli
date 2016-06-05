76fdac25e208ab8c6148538753f0573ed8f451ca
Jacek Szpot <maligree@gmail.com>
2016-06-05 11:32:17 +0200

    requires -> install_requires

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     author_email='maligree@gmail.com',
     url='https://github.com/exana/mysqli',
     scripts=['scripts/mysqli'],
-    requires=['termcolor']
+    install_requires=['termcolor']
 )
